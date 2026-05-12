c5bdf0d2201812d8c4c946914522a86b5b567f1c
devaneyj3 <jordandevaney28@gmail.com>
2026-05-12 19:18:24 -0400

    edit styles on profile page

--- components/Movies/FavList/FavList.js
+++ components/Movies/FavList/FavList.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import styles from "./FavList.module.scss";
-
+import { Heart, List, Star, X } from 'lucide-react';
 import { dateFormatter } from "@/utils/dateFormater";
 import { useMovies } from "@/context/moviesContest";
 
@@ -25,33 +25,51 @@ export default function FavList({ movieDetails }) {
     <>
       <div className={styles.movieContainer}>
         {fonudMovies && fonudMovies.map((fonudMovie) => (
+
           <div key={fonudMovie.id} className={styles.movie}>
-            <div className={styles.imageContainer}>
-              <Image
-                src={`https://image.tmdb.org/t/p/w500${fonudMovie.poster_path}`}
-                width={150}
-                height={200}
-                alt={`${fonudMovie.title} Poster`}
-                className={styles.image}
-              />
-            </div>
-            <div className={styles.details}>
-              <div className={styles.detailHeader}>
-                <h2>{fonudMovie.title}</h2>
-                <div className={styles.attributes}>
-                  <span>{dateFormatter(fonudMovie.release_date)}</span>
-                  {fonudMovie.genres?.length > 0 &&
-                    fonudMovie.genres.map((genre) => (
-                      <span key={genre.id}>{genre.name}</span>
-                    ))}
-                  <span>{fonudMovie.runtime} min</span>
+            <div className={styles.movieInfo}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500${fonudMovie.poster_path}`}
+                  width={150}
+                  height={200}
+                  alt={`${fonudMovie.title} Poster`}
+                  className={styles.image}
+                />
+              </div>
+              <div className={styles.details}>
+                <div className={styles.detailHeader}>
+                  <h2>{fonudMovie.title}</h2>
+                  <div className={styles.attributes}>
+                    <p>{dateFormatter(fonudMovie.release_date)}</p>
+                  </div>
+                </div>
+                <div className={styles.synopsis}>
+                  <p >{fonudMovie.overview}</p>
                 </div>
               </div>
-              <span className={styles.value}>{fonudMovie.vote_average}/10</span>
-              <div className={styles.synopsis}>
-                <h2>{fonudMovie.tagline}</h2>
-                <p >{fonudMovie.overview}</p>
+            </div>
+            <div className={styles.actionBar}>
+              <div className={styles.column}>
+                <div className={styles.actionBox}>
+                  <div className={styles.actionIcon}><Star /></div>
+                  <h2>Rate it!</h2>
+                </div>
+              <div className={styles.actionBox}>
+                <div className={styles.actionIcon}><Heart /></div>
+                <h2>Favorite</h2>
+                </div>
               </div>
+              <div className={styles.column}>
+              <div className={styles.actionBox}>
+                <div className={styles.actionIcon}><List /></div>
+                <h2>Add to list</h2>
+              </div>
+              <div className={styles.actionBox}>
+                <div className={styles.actionIcon}><X /></div>
+                <h2>Remove</h2>
+              </div>
+                </div>
             </div>
           </div>
         ))}
